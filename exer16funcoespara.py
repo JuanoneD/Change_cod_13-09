9acad9dab3d9eca3897b7bb3dbb96f54626c5435
juanoned <juandiego.jd42@gmail.com>
2024-09-13 15:13:42 -0300

    exer 9

---
+++ exer16funcoespara.py
@@ -0,0 +1,6 @@
+#https://github.com/Andredev-dias/Base-de-algoritmos-em-C-v3/blob/master/exer16funcoespara.c
+
+num1 = int(input('escreva o primeiro numero'))
+num2 = int(input('escreva o segundo nuemro'))
+
+print("soma enre {}+{} é {}".format(num1,num2,num1+num2))
