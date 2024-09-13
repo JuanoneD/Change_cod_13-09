6c5da397416e9884910a68d891d0ae9a0f8d5927
juanoned <juandiego.jd42@gmail.com>
2024-09-13 16:29:36 -0300

    exer 17

---
+++ vetordeestruturaulapratica.py
@@ -0,0 +1,15 @@
+#https://github.com/Andredev-dias/Base-de-Algoritmos-em-C_v2/blob/master/vetordeestruturaulapratica.c
+
+candidatos = []
+
+for i in range(3):
+    candato={}
+    candato['codigo'] = int(input("Escreva o codigo do candidato"))
+    candato['salario'] = float(input("Escreva o salario do candidato"))
+    candato['email'] = input("Escreva o email do candidato")
+    candidatos.append(candato)
+    
+for index,i in candidatos:
+    print(index)
+    print(i)
+    
